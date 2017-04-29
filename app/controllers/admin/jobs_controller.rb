b08b83fff3e758c6c2af0f7ed4a2713331927a9b
xuxin <xuxin321@hotmail.com>
2017-04-29 21:55:43 +0800

    implement step5

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,6 +1,22 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   before_action :require_is_admin
+  layout "admin"
+
+  def publish
+    @job = Job.find(params[:id])
+    @job.publish!
+
+    redirect_to :back
+  end
+
+  def hide
+    @job = Job.find(params[:id])
+
+    @job.hide!
+
+    redirect_to :back
+  end
 
   def show
     @job = Job.find(params[:id])
@@ -47,8 +63,10 @@ class Admin::JobsController < ApplicationController
 
   private
 
+
   def job_params
     params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
+
 
 end
